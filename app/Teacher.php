7ac54a2eb4807820dd51ddbcfee1e2afe692c87b
Nasla Nafir <mnfathimanasla97@gmail.com>
2021-02-01 11:46:57 +0530

    SMS project

---
+++ app/Teacher.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Teacher extends Model
+{
+    public function user()
+    {
+        return $this->hasOne(User::class, 'id', 'user_id');
+    }
+
+    public function class()
+    {
+        return $this->hasOne(Classe::class);
+    }
+
+    public function timetable()
+    {
+        return $this->hasMany(TimeTable::class);
+    }
+
+    public function subjectGroup()
+    {
+        return $this->hasOne(SubjectGroup::class, 'id', 'subject_group_id');
+    }
+}
